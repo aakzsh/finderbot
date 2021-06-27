b6999f50bfd16fe841ccbf1e3a5fa6a3a36535dd
Aakash Shrivastava <69726390+aakzsh@users.noreply.github.com>
2021-06-27 10:12:05 +0530

    add audio recognition

--- main.py
+++ main.py
@@ -1,37 +1,43 @@
-import discord
+from jina import Document
+import numpy as np
+import json
+# d = Document()
+
+# d.text = 'hello world'
+# print(d4)
+
 from discord.ext import commands
+import requests
 
 
 bot = commands.Bot(command_prefix='$')
-from discord.ext.commands import Bot
 
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
 
 @bot.command()
-async def frooti(ctx):
-    await ctx.send('go brr')
-
-@bot.command()
-async def cute(ctx):
-    await ctx.send('no you ❤ :pepeOK: ')
+async def attack(ctx):
+    await ctx.send('🥺\nhelllllll-lo')
 
 @bot.command()
 async def search(ctx):
-    await ctx.send('send audio file or smth :PepeYikes:')
+    attachment_url = ctx.message.attachments[0].url
+    file_request = requests.get(attachment_url)
+    d4 = Document(content=ctx.message.attachments[0].url)
+    
+    data = {
+    'api_token': '<api token here>',
+    'url': d4.uri,
+    'return': 'apple_music,spotify',
+    }
+    result = requests.post('https://api.audd.io/', data=data)
+    workpwease = json.loads(result.text)
+    artist = workpwease['result']['artist']
+    song = workpwease['result']['title']
+    link = workpwease['result']['song_link']
+    await ctx.send(song + ", by " + artist + ".  Here's the link to enjoy your music(without ad ofc, coz we aint no spotify)  " +link)
 
-@bot.command()
-async def attack(ctx):
-    await ctx.send('🥺🥺🥺🥺🥺')
+# channel.send(file=discord.File('my_file.png'))
 
-@bot.command()
-async def aakash(ctx):
-    await ctx.send('good guy bang')
-
-
-
-
-
-
-bot.run('bro')
+bot.run('<bot token here>')
